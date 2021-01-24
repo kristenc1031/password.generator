7704c4d011f7a180bba16d4bef0afcedcabdc825
Kristen Campbell <kristenc1031@gmail.com>
2021-01-23 22:07:40 -0700

    modified script.js

--- script.js
+++ script.js
@@ -5,10 +5,31 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//i did everything below 
+for (var i = 0; i <= password; i++ ){
+  password = passwordText + values(Math.floor(Math.random)) * Math.floor(vaules.length)
+}
+
+var symbols = '!@#$%^&*()'; 
+
+console.log(Math.floor(Math.random() * 26) + 97); 
+
+console.log(Math.floor(Math.random() * 26) + 65); 
+
+console.log(Math.floor(Math.random() * 10) + 48); 
+
+function getRandomSymbol(){
+  var symbol = '!@#$%^&*()';
+  return symbol[Math.floor(Math.random()*symbol.length)];
+}
+
+
+
